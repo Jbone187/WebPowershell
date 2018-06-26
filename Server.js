8e93bd5ddca14882c4cbc9f9e934ac89898d3d40
Jasen Green <jgreen@xlanes.lcl>
2018-06-26 08:57:07 -0700

    Server.js

---
+++ Server.js
@@ -0,0 +1,35 @@
+let express=require('express');
+
+let bodyParser = require('body-parser');
+
+let powershell = require('./runpower');
+
+let app=express();
+
+/*------------------Routing Started ------------------------*/
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+
+
+/*Handle Post Request*/
+app.post('/',function(req,res){
+
+powershell();
+console.log('Hello');
+res.sendStatus(200);
+ 
+});
+
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(3000,function(){
+
+    console.log("Node is Running on Port 3000");
+
+});
---
+++ runpower.js
@@ -0,0 +1,22 @@
+module.exports = function () {
+
+const shell = require('node-powershell');
+ 
+let ps = new shell({
+  executionPolicy: 'Bypass',
+  noProfile: true
+});
+ 
+let power = 'ls';
+
+ps.addCommand(power)
+ps.invoke()
+.then(output => {
+  console.log(output);
+})
+.catch(err => {
+  console.log(err);
+  ps.dispose();
+})
+
+};
